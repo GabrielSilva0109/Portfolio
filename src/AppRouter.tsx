04a755d6da3fe29d8d4bb470c1dea1040579cd39
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-05-25 19:20:55 -0300

    1

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home'
 
 const AppRouter: React.FC = () => {
     return (
         <>
+         <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            
+        </Routes>
+
+     </Router>
+         
         </>
     )
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <>
+         
+         x\zz
+        </>
+    )
+}
+
+export default Home
