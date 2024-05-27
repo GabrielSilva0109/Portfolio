276cd16aeb18801d71d2c4025dc76001ea767dd8
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-27 12:28:12 -0300

    1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
   display: flex;
@@ -7,17 +8,50 @@ const Container = styled.div`
   align-items: center;
   background-color: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
-  border-bottom: 1px solid ${({ theme }) => theme.toggleBorder};
 `
 
 const ToggleButton = styled.button`
   background: ${({ theme }) => theme.background};
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
+  border: none;
   border-radius: 30px;
   cursor: pointer;
   font-size: 0.8rem;
-  padding: 0.6rem;
+  padding: 8px;
   color: ${({ theme }) => theme.text};
+  font-weight: bold;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: ${({ theme }) => theme.text}; 
+    color: ${({ theme }) => theme.background}; 
+  }
+`
+
+const Title = styled.h2`
+    font-size: 1.5rem;
+    color: #1e30f3;
+    
+`
+
+const Nav = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  margin: 0 10px;
+`
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.1rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.text};
+  transition: .5s;
+
+  &:hover {
+    color: #1e30f3;
+  }
 `
 
 interface HeaderProps {
@@ -25,10 +59,25 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+
   return (
     <Container>
-      <h1>My App</h1>
-      <ToggleButton onClick={toggleTheme}>Toggle Theme</ToggleButton>
+      <Title>Gabriel Silva</Title>
+      <Nav>
+        <NavItem>
+          <NavLink to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/projetos">Habilidades</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/contato">Projetos</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/habilidades">Contato</NavLink>
+        </NavItem>
+      </Nav>
+      <ToggleButton onClick={toggleTheme}></ToggleButton>
     </Container>
   )
 }
--- src/components/Styles/GlobalStyles.ts
+++ src/components/Styles/GlobalStyles.ts
@@ -1,10 +1,32 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+Sinhala:400,700');
+
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    font-family: 'Noto Sans Sinhala', Arial, Roboto, sans-serif;
+    font-weight: 400;
     transition: all 0.50s linear;
   }
-`
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Noto Sans Sinhala', Arial, Roboto, sans-serif;
+    font-weight: 700;
+  }
+
+  html { font-size: 100%; } /* 16px */
+
+  h1 { font-size: 4.210rem; } /* 67.36px */
+
+  h2 { font-size: 3.158rem; } /* 50.56px */
+
+  h3 { font-size: 2.369rem; } /* 37.92px */
+
+  h4 { font-size: 1.777rem; } /* 28.48px */
+
+  h5 { font-size: 1.333rem; } /* 21.28px */
+
+  small { font-size: 0.750rem; } /* 12px */
+`;
