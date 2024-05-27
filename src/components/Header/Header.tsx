473c65ff6edd6d3895135d4f8586fb6a9baa8149
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-27 10:31:01 -0300

    1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import  styled  from 'style-components'
 
-const Container 
 
 const Header: React.FC = () => {
     return (
-        <Container>
-
-        </Container>
-       
+       <>
+       </>
     )
 }
 
