05e9261edcf589bf75769923fb697a6902605eba
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-27 11:52:05 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
-import  styled  from 'style-components'
+import styled from 'styled-components'
 
-const Header: React.FC = () => {
-    return (
-       <>
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: ${({ theme }) => theme.body};
+  color: ${({ theme }) => theme.text};
+  border-bottom: 1px solid ${({ theme }) => theme.toggleBorder};
+`
 
-       
-       </>
-    )
+const ToggleButton = styled.button`
+  background: ${({ theme }) => theme.background};
+  border: 2px solid ${({ theme }) => theme.toggleBorder};
+  border-radius: 30px;
+  cursor: pointer;
+  font-size: 0.8rem;
+  padding: 0.6rem;
+  color: ${({ theme }) => theme.text};
+`
+
+interface HeaderProps {
+  toggleTheme: () => void
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+  return (
+    <Container>
+      <h1>My App</h1>
+      <ToggleButton onClick={toggleTheme}>Toggle Theme</ToggleButton>
+    </Container>
+  )
 }
 
 export default Header
