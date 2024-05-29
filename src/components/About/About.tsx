5eae41e70ba14ff3c6633bb1fdaa210052cff86a
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-05-29 14:57:56 -0300

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import Header from '../Header/Header';
+import styled from 'styled-components';
 
 interface AboutProps {
   toggleTheme: () => void;
   theme: string; 
 }
 
+const Container = styled.div`
+
+
+`
+
 const About: React.FC<AboutProps> = ({ toggleTheme, theme }) => {
   return (
     <div>
       <Header toggleTheme={toggleTheme} theme={theme} />
-      <h2>About Page</h2>
+      <Container>
+
+      </Container>
     </div>
   );
 }
