f0c73d5d981ba666688b4f72ee43f66a47a8985c
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-08-01 12:54:19 -0300

    Update Experience.tsx

--- src/components/About/Experience.tsx
+++ src/components/About/Experience.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 
-interface HomeProps {
-    toggleTheme: () => void;
-    theme: string; 
-}
-
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   padding: 15px;
-  flex-direction: column;
   gap: 40px;
-  margin-top: 120px ;
-  margin: 0;
-  padding: 0;
+  margin-top: 120px;
+`
+
+const TimelineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 80%;
+`
+
+const Line = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 2px;
+  background-color: #3584A7;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
 `
 
 const Content = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  padding: 15px;
-  margin: 0;
-  padding: 0;
+  margin-bottom: 40px;
+  width: 100%;
+  position: relative;
+  padding: 0 20px;
 `
 
 const Title = styled.h1`
@@ -37,15 +47,15 @@ const Title = styled.h1`
 `
 
 const SubTitle = styled.h3`
- background-image: linear-gradient(-225deg, rgb(10, 2, 247) 0%, #3584A7 51%, #30D2BE 100%);
+  background-image: linear-gradient(-225deg, rgb(10, 2, 247) 0%, #3584A7 51%, #30D2BE 100%);
   -webkit-background-clip: text;
   color: transparent;
   text-align: center;
-
   font-size: 2rem;
+  margin-bottom: 20px;
+  
   @media(max-width: 768px){
     font-size: 1.6rem;
-    margin-top: -40px;
   }
 `
 
@@ -60,25 +70,17 @@ const Skill = styled.h3`
 
 const Right = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
   flex-direction: column;
-  margin: 0;
-  padding: 0;
-  width: 60%
+  align-items: flex-start;
+  width: 50%;
 `
 
 const Left = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
   flex-direction: column;
-  margin: 0;
-  padding: 0;
-
-  width: 40%
+  align-items: center;
+  width: 50%;
+  position: relative;
 `
 
 const Ball = styled.div`
@@ -86,47 +88,39 @@ const Ball = styled.div`
   height: 20px;
   border-radius: 50%;
   background-color: #3584A7;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
 `
-
-const Line = styled.div`
-  height: 100px;
-  width: 2px;
-  background-color: #3584A7;
-`
-
 
 const Experience: React.FC = () => {
-    return (
-        <Container>
-            <SubTitle>Experiencia</SubTitle>
-
-            <Content>
-              <Right>
-                <Title>Desenvolvedor Fullstack</Title>
-                <Skill>Typescript | React | Firebase</Skill>
-              </Right>
-
-
-              <Left>
-                <Ball></Ball>
-                <Line></Line>
-              </Left>
-              
-            </Content>
-            <Content>
-              <Right>
-                <Title>Desenvolvedor Fullstack</Title>
-                <Skill>Java | Spring | React</Skill>
-              </Right>
-
-              <Left>
-                <Ball></Ball>
-                <Line></Line>
-              </Left>
-              
-            </Content>
-        </Container>
-    )
+  return (
+    <Container>
+      <SubTitle>Experiência</SubTitle>
+      <TimelineContainer>
+        <Line />
+        <Content>
+          <Right>
+            <Title>Desenvolvedor Fullstack</Title>
+            <Skill>Typescript | React | Firebase</Skill>
+          </Right>
+          <Left>
+            <Ball />
+          </Left>
+        </Content>
+        <Content>
+          <Right>
+            <Title>Desenvolvedor Fullstack</Title>
+            <Skill>Java | Spring | React</Skill>
+          </Right>
+          <Left>
+            <Ball />
+          </Left>
+        </Content>
+      </TimelineContainer>
+    </Container>
+  )
 }
 
 export default Experience
