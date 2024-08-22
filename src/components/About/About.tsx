166ef6d812578e369d0a6f2d7699be0a734d18af
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-08-22 16:44:59 -0300

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -320,6 +320,7 @@ const About: React.FC<AboutProps> = ({ toggleTheme, theme }) => {
 
   return (
     <div>
+    
       <Header toggleTheme={toggleTheme} theme={theme} />
       <Container>
         <Left>
