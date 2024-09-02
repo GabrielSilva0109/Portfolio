b24091211f36126bc2f182a243ad32689fc21ec9
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-09-02 08:23:26 -0300

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -302,6 +302,7 @@ const skillsData = [
   { name: 'Spring', percentage: 70 },
 ]
 
+
 const titles = ['Programador', 'Desenvolvedor', 'Estudante']
 
 const About: React.FC<AboutProps> = ({ toggleTheme, theme }) => {
